4f5820e29e5bd9f81ba310758c6fe450ecf06e45
titvvn <nhatluong0987.email@gmail.com>
2026-01-08 04:13:07 +0700

    again code old

--- frontend/src/admin/helpers/filterListFood.js
+++ frontend/src/admin/helpers/filterListFood.js
@@ -1,41 +1,29 @@
-import { useState ,useEffect} from "react";
+const nameList = [
+    { id: 1, title: "All"  },
+    { id: 2, title: "Lẩu" ,category :"lau"},
+    { id: 3, title: "Nướng",category :"nuong" },
+    { id: 4, title: "Cơm" ,category :"com"},
+    { id: 5, title: "Đồ rán",category :"do_ran" },
+    { id: 6, title: "Đồ hấp & Luộc" ,category :"do_hap_luoc"},
+    { id: 7, title: "Phở & Bún ...." ,category :"lau"},
+    { id: 8, title: "Bánh rán ...." ,category :"banh_ran"},
+    { id: 9, title: "Ăn vặt" ,category :"lau"},
+    { id: 10, title: "Đồ uống" ,category :"do_uong"},
+    { id: 11, title: "Đồ ngoại" ,category :"lau"},
+    { id: 12, title: "Combo" ,category :"combo"},
+]
+
 
 function FilterListFood({ activeTab, onTabClick }) {
-    const [listNameFood, setListNameFood] = useState([
-        
-    ]);
-
-    const apiListFood = () => {
-        fetch("/api/admin/category")
-            .then((res) => res.json())
-            .then((data) => {
-                setListNameFood(data);
-            })
-            .catch((err) => {
-                console.error("Lỗi khi lấy danh mục món ăn:", err);
-            });
-    }
- 
-
-    useEffect(() => {
-        apiListFood()
-    }, [])
-
     return (
         <div style={{ display: "flex", gap: "10px" }}>
-              <button
-                    onClick={() => onTabClick(null, null)}
-                    className={`admin-btn ${activeTab === null ? "admin-primary" : ""}`}
-                >
-                    Tất cả
-                </button>
-            {listNameFood.map((tab) => (
+            {nameList.map((tab) => (
                 <button
-                    key={tab._id}
-                    className={`admin-btn ${activeTab === tab._id ? "admin-primary" : ""}`}
-                    onClick={() => onTabClick(tab.category, tab._id)}
+                    key={tab.id}
+                    className={`admin-btn ${activeTab === tab.id ? "admin-primary" : ""}`}
+                    onClick={() => onTabClick(tab.category, tab.id)}
                 >
-                    {tab.name}
+                    {tab.title}
                 </button>
             ))}
         </div>
