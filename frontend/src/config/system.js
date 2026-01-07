5ff9c01d25fe1cc21bf2c883859417be12a647ac
titvvn <nhatluong0987.email@gmail.com>
2026-01-08 02:57:31 +0700

    fix no hard url

--- frontend/src/config/system.js
+++ frontend/src/config/system.js
@@ -1,2 +1,3 @@
 // src/config.js
-export const prefixAdmin = 'http://localhost:3000/';
+// export const prefixAdmin = 'http://localhost:3000/'; // hạn chế fix cứng đường dẫn
+export const prefixAdmin = '/';
